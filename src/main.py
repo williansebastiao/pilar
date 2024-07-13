f45b517726ddc834d3623a5e86d45f07826104fa
Willians Pereira <willians@4vconnect.com>
2024-07-13 11:00:07 -0300

    added base exception and schemas

--- src/main.py
+++ src/main.py
@@ -1,15 +1,7 @@
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
+
 from src.routes.api import router as api_router
 
-app = FastAPI()
-app.include_router(api_router)
+app = FastAPI(title="Pilar")
 
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    errors = exc.errors()
-    return JSONResponse(
-        status_code=422,
-        content={"message": errors[0]['msg']}
-    )
+app.include_router(api_router)
--- src/routes/api.py
+++ src/routes/api.py
@@ -2,25 +2,26 @@ from fastapi import APIRouter, Response
 
 from src.helpers.http_response import HttpResponse
 from src.helpers.status_code import StatusCode
-from src.schemas.vowel_count import VowelCount
+from src.schemas.vowel_count_schema import VowelCountSchema
+from src.schemas.sort_schema import SortSchema
 
 router = APIRouter(prefix="/api")
 
 
 @router.get('/', tags=['Health Check'])
 async def index() -> Response:
-    content = {'message': 'Hello World'}
+    content = {'message': "It's running..."}
     return HttpResponse(status_code=StatusCode.OK, content=content).json()
 
 
 @router.post('/vowel_count', tags=['Counts vowels in words'])
-async def vowel_count(body: VowelCount) -> Response:
+async def vowel_count(body: VowelCountSchema) -> Response:
     print(body.words)
     content = {'message': 'Counts vowels in words'}
     return HttpResponse(status_code=StatusCode.OK, content=content).json()
 
 
 @router.post('/sort', tags=['Order words'])
-async def order_words() -> Response:
+async def order_words(body: SortSchema) -> Response:
     content = {'message': 'Order words'}
     return HttpResponse(status_code=StatusCode.OK, content=content).json()
